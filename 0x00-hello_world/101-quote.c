476faa9c8d6500fb7cc3e55c5ce24e48db602661
Pierre Dureau <dureaupierre53@gmail.com>
2022-02-03 11:50:09 +0100

    Print without printf & puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-static const char hello[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
+char hello[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
 fwrite(hello, sizeof(hello) - 1, 1, stdout);
-return (0);
+return (1);
 }
