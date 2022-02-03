a252a8e25eb1ca3c4e68cafe11c9f93926e5e3c5
Pierre Dureau <dureaupierre53@gmail.com>
2022-02-03 10:51:03 +0100

    Use puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - print a string
+ *
+ * Return: a string
+ */
 int main(void)
 {
-  char str[] = "\"Programming is like building a multilingual puzzle";
-  puts(str);
-  return (0);
+char str[] = "\"Programming is like building a multilingual puzzle";
+puts(str);
+return (0);
 }
