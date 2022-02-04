dede932ad23ef6675323c9cbd0b7ce1c20195800
Pierre Dureau <dureaupierre53@gmail.com>
2022-02-04 12:26:03 +0100

    Print the number of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - Print the numbers of base 16
+ *return - 0
+ */
+int main(void)
+{
+	int i;
+	char l;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i % 10 + '0');
+	}
+	for (l = 'a'; l <= 'f'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+
+	return (0);
+}
