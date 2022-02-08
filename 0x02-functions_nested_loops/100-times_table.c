ac60822377a78cae540cbc415f067988e83fa674
Pierre Dureau <dureaupierre53@gmail.com>
2022-02-08 14:04:29 +0100

    Print the n table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,38 @@
 #include "main.h"
 
 /**
- * print_time_table - print the n table
+ * print_diff_len - Print value with different length
+ *
+ * @r: the value
+ */
+
+void print_diff_len(int r)
+{
+	if (r < 10)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(r + '0');
+	}
+	else if (r < 100)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(r / 10 + '0');
+		_putchar(r % 10 + '0');
+	}
+	else
+	{
+		_putchar(' ');
+		_putchar(r / 100 + '0');
+		_putchar((r / 10) % 10 + '0');
+		_putchar(r % 10 + '0');
+	}
+}
+
+/**
+ * print_times_table - print the n table
  * @n: n
  */
 
@@ -20,27 +51,7 @@ void print_times_table(int n)
 			{
 				r = i * j;
 
-				if (r < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(r + '0');
-				}
-				else if (r < 100)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(r / 10 + '0');
-					_putchar(r % 10 + '0');
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar(r / 100 + '0');
-					_putchar((r / 10) % 10 + '0');
-					_putchar(r % 10 + '0');
-				}
+				print_diff_len(r);
 
 				if (j != n)
 				{
