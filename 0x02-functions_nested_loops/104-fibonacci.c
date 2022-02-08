3f7168898c2a2d5b6d80c9b1d238d263f5f8a59d
Pierre Dureau <dureaupierre53@gmail.com>
2022-02-08 19:17:51 +0100

    Fix

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,6 +2,56 @@
 #include <stdio.h>
 
 /**
+ * print_two_long - print the Fibonacci value with a 2 longs
+ *
+ * @i: compteur de la liste
+ * @fib1: surplus du 1er long
+ * @fib2: 1er long
+ * @fib3: surplus du 2eme long
+ * @fib4: 2eme long
+ */
+
+void print_two_long(int i,
+					unsigned long fib1,
+					unsigned long fib2,
+					unsigned long fib3,
+					unsigned long fib4)
+{
+	for (i = i; i < 96; i++)
+	{
+		if (i % 2 == 0)
+		{
+			fib1 = fib1 + fib3;
+			fib2 = fib2 + fib4;
+		}
+		else
+		{
+			fib3 = fib3 + fib1;
+			fib4 = fib4 + fib2;
+		}
+
+
+		if (fib2 > 10000000000)
+		{
+			fib1 = fib1 + (fib2 / 10000000000);
+		}
+		else if (fib4 > 10000000000)
+		{
+			fib3 = fib3 + (fib4 / 10000000000);
+		}
+
+		if (i % 2 == 0)
+		{
+			printf("%lu%lu\n", fib1, fib2);
+		}
+		else
+		{
+			printf("%lu%lu\n", fib3, fib4);
+		}
+	}
+}
+
+/**
  * main - Print the first 98 Fibonacci numbers
  *
  * Return: 0
@@ -46,39 +96,6 @@ int main(void)
 				fib1 = fib1 / 10000000000;
 				break;
 			}
-		}
-	}
-
-	for (i = i; i < 96; i++)
-	{
-		if (i % 2 == 0)
-		{
-			fib1 = fib1 + fib3;
-			fib2 = fib2 + fib4;
-		}
-		else
-		{
-			fib3 = fib3 + fib1;
-			fib4 = fib4 + fib2;
-		}
-
-
-		if (fib2 > 10000000000)
-		{
-			fib1 = fib1 + (fib2 / 10000000000);
-		}
-		else if (fib4 > 10000000000)
-		{
-			fib3 = fib3 + (fib4 / 10000000000);
-		}
-
-		if (i % 2 == 0)
-		{
-			printf("%lu%lu\n", fib1, fib2);
-		}
-		else
-		{
-			printf("%lu%lu\n", fib3, fib4);
 		}
 	}
 	return (0);
