afedbf75bd5994ccc8bfe1785cfc33b7a4f5813f
Pierre Dureau <dureaupierre53@gmail.com>
2022-02-08 10:23:06 +0100

    Print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the alphabet x10
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
