b3670fb1d02eed8aa16d939baefb852b7b9cfe80
Pierre Dureau <dureaupierre53@gmail.com>
2022-02-08 14:08:30 +0100

    Print the n table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,6 +10,7 @@ void print_diff_len(int r)
 {
 	if (r < 10)
 	{
+		_putchar(',');
 		_putchar(' ');
 		_putchar(' ');
 		_putchar(' ');
@@ -17,6 +18,7 @@ void print_diff_len(int r)
 	}
 	else if (r < 100)
 	{
+		_putchar(',');
 		_putchar(' ');
 		_putchar(' ');
 		_putchar(r / 10 + '0');
@@ -24,6 +26,7 @@ void print_diff_len(int r)
 	}
 	else
 	{
+		_putchar(',');
 		_putchar(' ');
 		_putchar(r / 100 + '0');
 		_putchar((r / 10) % 10 + '0');
@@ -38,14 +41,13 @@ void print_diff_len(int r)
 
 void print_times_table(int n)
 {
-	if (0 <= n && n <= 15)
+	if (n >= 0 && n <= 15)
 	{
 		int i, j, r;
 
 		for (i = 0; i <= n; i++)
 		{
 			_putchar('0');
-			_putchar(',');
 
 			for (j = 1; j <= n; j++)
 			{
@@ -53,10 +55,6 @@ void print_times_table(int n)
 
 				print_diff_len(r);
 
-				if (j != n)
-				{
-					_putchar(',');
-				}
 			}
 			_putchar('\n');
 		}
