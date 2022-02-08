2c6ce761c3ff12a3e69dfc4b4cae8f66123000da
Pierre Dureau <dureaupierre53@gmail.com>
2022-02-08 19:07:04 +0100

    Fix

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,32 +10,76 @@
 int main(void)
 {
 	int i;
-	unsigned long n, m;
+	unsigned long fib1, fib2, fib3, fib4;
 
-	n = 1;
-	printf("%lu, ", n);
-	m = 1;
+	fib1 = 1;
+	printf("%lu\n", fib1);
+	fib2 = 1;
 
 	for (i = 0; i < 97; i++)
 	{
 		if (i % 2 == 0)
 		{
-			n = n + m;
-			printf("%lu", n);
+			fib1 = fib1 + fib2;
+			printf("%lu\n", fib1);
+
+			if (fib1 > 10000000000)
+			{
+				fib4 = fib1 % 10000000000;
+				fib3 = fib1 / 10000000000;
+				fib1 = fib2 / 10000000000;
+				fib2 = fib2 % 10000000000;
+				break;
+			}
+
 		}
 		else
 		{
-			m = m + n;
-			printf("%lu", m);
-		}
+			fib2 = fib1 + fib2;
+			printf("%lu\n", fib2);
 
-		if (i != 96)
-		{
-			printf(", ");
+			if (fib2 > 10000000000)
+			{
+				fib4 = fib2 % 10000000000;
+				fib3 = fib2 / 10000000000;
+				fib2 = fib1 % 10000000000;
+				fib1 = fib1 / 10000000000;
+				break;
+			}
 		}
 	}
 
-	printf("\n");
+	for (i = i; i < 96; i++)
+	{
+		if (i % 2 == 0)
+		{
+			fib1 = fib1 + fib3;
+			fib2 = fib2 + fib4;
+		}
+		else
+		{
+			fib3 = fib3 + fib1;
+			fib4 = fib4 + fib2;
+		}
 
+
+		if (fib2 > 10000000000)
+		{
+			fib1 = fib1 + (fib2 / 10000000000);
+		}
+		else if (fib4 > 10000000000)
+		{
+			fib3 = fib3 + (fib4 / 10000000000);
+		}
+
+		if (i % 2 == 0)
+		{
+			printf("%lu%lu\n", fib1, fib2);
+		}
+		else
+		{
+			printf("%lu%lu\n", fib3, fib4);
+		}
+	}
 	return (0);
 }
