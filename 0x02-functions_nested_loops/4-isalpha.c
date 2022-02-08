5ff8890f5aa38d991a568e196cbd8140761c5059
Pierre Dureau <dureaupierre53@gmail.com>
2022-02-08 11:01:06 +0100

    Check for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isalpha - check for alphabetix character
+ * @c: the character to check
+ *
+ * Return: On success 1
+ */
+
+int _isalpha(int c)
+{
+	if (isalpha(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
