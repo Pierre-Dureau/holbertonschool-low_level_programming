c5df91273ca28f5ed6a7dadb61a0e81706af9ff7
Pierre Dureau <dureaupierre53@gmail.com>
2022-02-08 15:04:10 +0100

    Print the sum of the even-valued terms of the Fibonacci sequence

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Print the sum of the even-valued terms of the Fibonacci sequence
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	long int n, m, p;
+
+	n = 1;
+	m = 1;
+	p = 0;
+
+	while (n < 4000000 && m < 4000000)
+	{
+		if (i % 2 == 0)
+		{
+			n = n + m;
+			if (n % 2 == 0)
+			{
+				p += n;
+				printf("%ld, ", p);
+			}
+		}
+		else
+		{
+			m = m + n;
+			if (m % 2 == 0)
+			{
+				p += m;
+				printf("%ld, ", p);
+			}
+		}
+		i++;
+	}
+
+	printf("\n");
+
+	return (0);
+}
