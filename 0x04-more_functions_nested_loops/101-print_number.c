7c2d7997b53ed8239c548044f618911b56f8a25d
Pierre Dureau <dureaupierre53@gmail.com>
2022-02-10 14:10:57 +0100

    Print any number with putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,20 +1,23 @@
 #include "main.h"
 
 /**
- * main - print an integer with putchar
+ * print_number - print an integer with putchar
  *
  * @n: The integer
  */
 
 void print_number(int n)
 {
-	if (n < 0) {
+	if (n < 0)
+	{
 		_putchar('-');
 		n = n * -1;
 	}
 
 	if (n / 10)
+	{
 		print_number(n / 10);
+	}
 
 	_putchar(n % 10 + '0');
 }
