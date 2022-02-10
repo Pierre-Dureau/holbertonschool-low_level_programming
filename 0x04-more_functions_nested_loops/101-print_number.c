e7cb3e8a9c5c0764d415e192fbda2396335684e6
Pierre Dureau <dureaupierre53@gmail.com>
2022-02-10 14:18:12 +0100

    Print any number with putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -18,12 +18,13 @@ void print_number(int n)
 	{
 		_putchar('0');
 	}
-
-
-	if (n / 10)
+	else
 	{
-		print_number(n / 10);
-	}
+		if (n / 10)
+		{
+			print_number(n / 10);
+		}
 
-	_putchar(n % 10 + '0');
+		_putchar(n % 10 + '0');
+	}
 }
