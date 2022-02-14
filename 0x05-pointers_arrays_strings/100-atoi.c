6896910cd8d8309afdb0cfed89f2f3db0de2a79c
Pierre Dureau <dureaupierre53@gmail.com>
2022-02-14 14:32:30 +0100

    Fix int min excpetion

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -21,13 +21,27 @@ int _atoi(char *s)
 		}
 		else if (*s >= '0' && *s <= '9')
 		{
-			res = (*s - '0') + (res * 10);
+			if (res * 10 >= 2147483640)
+			{
+				if ((neg % 2 != 0) && (*s == '8'))
+				{
+					res = -2147483648;
+					neg = 0;
+				}
+				else
+				{
+					res = (*s - '0') + (res * 10);
+				}
+			}
+			else
+			{
+				res = (*s - '0') + (res * 10);
+			}
 		}
 		else if (res > 0)
 		{
 			break;
 		}
-
 	}
 
 	if (neg % 2 != 0)
