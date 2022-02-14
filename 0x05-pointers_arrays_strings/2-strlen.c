bf76895f18cc621f12c791277ed573caa80d2c6f
Pierre Dureau <dureaupierre53@gmail.com>
2022-02-14 01:46:12 +0100

    Return the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+/**
+ * _strlen - Return the length of a string
+ * @s: The string
+ *
+ * Return: The length of s
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
