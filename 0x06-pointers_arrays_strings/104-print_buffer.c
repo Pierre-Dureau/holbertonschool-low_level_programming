b68525143c004023f265cb590b6c2741adde6b9b
Pierre Dureau <dureaupierre53@gmail.com>
2022-02-17 12:39:18 +0100

    Print buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -16,36 +16,41 @@ void print_buffer(char *b, int size)
 
 	hexa = 0;
 
-	for (i = 0; i <= size / 10; i++)
-	{
-		printf("%08x: ", hexa);
-		hexa += 10;
-
-		for (j = 0; j < 10; j++)
-		{
-			if ((hexa + j) < (size + 10))
-				printf("%02x", *b);
-			else
-				printf("  ");
-			if ((j + 1) % 2 == 0)
-				printf(" ");
-			b++;
-		}
-
-		for (j = 0; j < 10; j++)
-			b--;
-
-		for (j = 0; j < 10; j++)
-		{
-			if ((hexa + j) < (size + 10))
-			{
-				if (isprint(*b))
-					printf("%c", *b);
-				else
-					printf(".");
-			}
-			b++;
-		}
+	if (size <= 0)
 		printf("\n");
+	else
+	{
+		for (i = 0; i <= size / 10; i++)
+		{
+			printf("%08x: ", hexa);
+			hexa += 10;
+
+			for (j = 0; j < 10; j++)
+			{
+				if ((hexa + j) < (size + 10))
+					printf("%02x", *b);
+				else
+					printf("  ");
+				if ((j + 1) % 2 == 0)
+					printf(" ");
+				b++;
+			}
+
+			for (j = 0; j < 10; j++)
+				b--;
+
+			for (j = 0; j < 10; j++)
+			{
+				if ((hexa + j) < (size + 10))
+				{
+					if (isprint(*b))
+						printf("%c", *b);
+					else
+						printf(".");
+				}
+				b++;
+			}
+			printf("\n");
+		}
 	}
 }
