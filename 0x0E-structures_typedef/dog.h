f6c03c096e0b7a5a9f3410a242613a6fb9ff15fa
Pierre Dureau <dureaupierre53@gmail.com>
2022-03-07 01:38:57 +0100

    Struct of a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - Attributs of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif /* MAIN_H */
