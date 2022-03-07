fb7c671a798bd6e9454889a92d1d3b9ba1924135
Pierre Dureau <dureaupierre53@gmail.com>
2022-03-07 02:17:21 +0100

    Update header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,13 +6,15 @@
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
- */
+*/
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif /* MAIN_H */
