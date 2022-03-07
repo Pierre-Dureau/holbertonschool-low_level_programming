857be510f7c279c433db77a35d5d92e1d547bb51
Pierre Dureau <dureaupierre53@gmail.com>
2022-03-07 17:19:50 +0100

    Fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,59 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - Return the length of a string
+ * @s: The string
+ *
+ * Return: The length of s
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * _strdup - copy of a string
+ *
+ * @str: The string
+ *
+ * Return: The string duplicated
+ */
+
+char *_strdup(char *str)
+{
+	int len, i;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = _strlen(str);
+
+	if (str)
+	{
+		s = malloc((len + 1) * sizeof(char));
+
+		if (s == NULL)
+			return (NULL);
+
+		for (i = 0; i < len; i++)
+			s[i] = str[i];
+	}
+	else
+		return (0);
+
+	return (s);
+}
+
+/**
  * new_dog - Create a new dog
  *
  * @name: Name of the dog
@@ -18,21 +71,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog == NULL)
 		return (NULL);
 
-	dog->name = name;
-	if (dog->name == NULL)
-	{
-		free(dog);
-		return (NULL);
-	}
-
 	dog->age = age;
-
-	dog->owner = owner;
+	dog->name = _strdup(name);
+	if (dog->name == NULL)
+		return (NULL);
+	dog->owner = _strdup(owner);
 	if (dog->owner == NULL)
-	{
-		free(dog->name);
-		free(dog);
-	}
-
+		return (NULL);
 	return (dog);
 }
