bd5d332a18c7026829c0b48c9d545fcc8960cd12
Pierre Dureau <dureaupierre53@gmail.com>
2022-03-09 13:10:14 +0100

    Fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,31 +10,33 @@
  */
 int main(int argc, char **argv)
 {
-	int (*ptr)(int, int);
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[3]);
-
-	if (argc == 4)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	ptr = get_op_func(argv[2]);
-
-	if (ptr == NULL)
+	else
 	{
-		printf("Error\n");
-		exit(99);
-	}
+		int (*ptr)(int, int);
+		int num1 = atoi(argv[1]);
+		int num2 = atoi(argv[3]);
 
-	if ((ptr == op_div || ptr == op_mod) && num2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
+		ptr = get_op_func(argv[2]);
 
-	printf("%d\n", ptr(num1, num2));
+		if (ptr == NULL)
+		{
+			printf("Error\n");
+			exit(99);
+		}
+
+		if ((ptr == op_div || ptr == op_mod) && num2 == 0)
+		{
+			printf("Error\n");
+			exit(100);
+		}
+
+		printf("%d\n", ptr(num1, num2));
+	}
 
 	return (0);
 }
