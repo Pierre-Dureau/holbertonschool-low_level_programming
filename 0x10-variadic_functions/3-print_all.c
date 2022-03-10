9324fc395fd02d6d352f196cb89d57b8acd8bcfb
Pierre Dureau <dureaupierre53@gmail.com>
2022-03-10 12:54:41 +0100

    Fix if null

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -15,33 +15,33 @@ void print_all(const char * const format, ...)
 
 	va_start(ap, format);
 
-	while (format[i] != '\0')
+	if (format)
 	{
-		switch (format[i])
+		while (format[i] != '\0')
 		{
-		case 'c':
-			printf("%s%c", sep, va_arg(ap, int));
-			break;
-		case 'i':
-			printf("%s%d", sep, va_arg(ap, int));
-			break;
-		case 'f':
-			printf("%s%f", sep, va_arg(ap, double));
-			break;
-		case 's':
-			s = va_arg(ap, char *);
-			if (s)
-				printf("%s%s", sep, s);
-			if (!s)
-				printf("(nil)");
-			break;
-		default:
+			switch (format[i])
+			{
+			case 'c':
+				printf("%s%c", sep, va_arg(ap, int));
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%s%f", sep, va_arg(ap, double));
+				break;
+			case 's':
+				s = va_arg(ap, char *);
+				s ? (printf("%s%s", sep, s)) : printf("(nil)");
+				break;
+			default:
+				i++;
+				continue;
+				break;
+			}
 			i++;
-			continue;
-			break;
+			sep = ", ";
 		}
-		i++;
-		sep = ", ";
 	}
 
 	printf("\n");
