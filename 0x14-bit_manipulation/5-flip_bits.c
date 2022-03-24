bada91153ff3b3855dd16a29c4ec178a3b98195f
Pierre Dureau <dureaupierre53@gmail.com>
2022-03-24 19:08:06 +0100

    returns the number of bits you would need to flip

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -16,7 +16,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	for (; n > 0 || m > 0; n >>= 1, m >>= 1)
 	{
 		if ((n & 1) != (m & 1))
-			count ++;
+			count++;
 	}
 
 	return (count);
