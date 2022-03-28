58d7394bc08b11e77747f65fed559d1f09ddcaab
Pierre Dureau <dureaupierre53@gmail.com>
2022-03-28 12:53:04 +0200

    Fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,7 +10,7 @@
 
 int main(int ac, char **av)
 {
-	int fd1, fd2, r, c1, c2;
+	int fd1, fd2, r, w, c1, c2;
 	char buf[1024];
 
 	if (ac != 3)
@@ -19,7 +19,7 @@ int main(int ac, char **av)
 		exit(97);
 	}
 	fd1 = open(av[1], O_RDONLY);
-	if (fd1 == -1)
+	if (fd1 < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
@@ -27,25 +27,25 @@ int main(int ac, char **av)
 	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	while ((r = read(fd1, buf, 1024)) > 0)
 	{
-		if (write(fd2, buf, r) != r || fd2 == -1)
+		w = write(fd2, buf, r);
+		if (w != r || fd2 < 0)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-			close(fd1);
 			exit(99);
 		}
 	}
-	if (r == -1)
+	if (r < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	c1 = close(fd1);
 	c2 = close(fd2);
-	if (c1 == -1 || c2 == -1)
+	if (c1 < 0 || c2 < 0)
 	{
-		if (c1 == -1)
+		if (c1 < 0)
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
-		if (c2 == -1)
+		if (c2 < 0)
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
 		exit(100);
 	}
