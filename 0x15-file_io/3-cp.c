7f84b07328566655db1bd2df87bdc9f4967bbd83
Pierre Dureau <dureaupierre53@gmail.com>
2022-03-28 12:51:18 +0200

    Fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -30,6 +30,7 @@ int main(int ac, char **av)
 		if (write(fd2, buf, r) != r || fd2 == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			close(fd1);
 			exit(99);
 		}
 	}
