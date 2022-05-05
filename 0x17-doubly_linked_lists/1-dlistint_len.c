cc26eeeaf463f1f2162123a95865f721628d4f20
Pierre Dureau <dureaupierre53@gmail.com>
2022-05-05 09:40:12 +0200

    List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * dlistint_len - Count number of node
+ *
+ * @h: head of the list
+ * Return: Size
+ */
+
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
