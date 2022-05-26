ac1255b9a187757601bbbe8b8d09471b92ffa119
Pierre Dureau <dureaupierre53@gmail.com>
2022-05-26 21:44:28 +0200

    creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,6 +10,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
+	unsigned long int i;
 
 	ht = malloc(sizeof(hash_table_t));
 	if (!ht)
@@ -19,6 +20,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (!ht->array)
 		return (NULL);
+
+	for (i = 0; i < ht->size; i++)
+		ht->array[i] = NULL;
 
 	return (ht);
 }
