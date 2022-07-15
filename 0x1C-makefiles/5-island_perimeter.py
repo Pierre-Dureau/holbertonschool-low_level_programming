50ad59edad4bb4c8c1bd179e4a70440b325bbc66
Pierre Dureau <dureaupierre53@gmail.com>
2022-07-15 16:52:04 +0200

    perimeter of an island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
+"""
+    Module for the funtion island_perimeter
+"""
+
 def island_perimeter(grid):
     """returns the perimeter of the island described in grid"""
     count = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
-                if grid[i + 1][j] == 0 or j == len(grid):
+                if grid[i + 1][j] == 0:
                     count += 1
-                if grid[i - 1][j] == 0 or j == 0:
+                if grid[i - 1][j] == 0:
                     count += 1
-                if grid[i][j + 1] == 0 or i == len(grid[0]):
+                if grid[i][j + 1] == 0:
                     count += 1
-                if grid[i][j - 1] == 0 or i == 0:
+                if grid[i][j - 1] == 0:
                     count += 1
 
     return count
