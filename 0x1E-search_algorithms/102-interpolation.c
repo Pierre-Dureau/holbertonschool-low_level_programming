45adfc494337a1f79559561d42f792bb0f6bb093
Pierre Dureau <dureaupierre53@gmail.com>
2022-08-30 15:32:16 +0200

    Fix code with recursion

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,5 +1,28 @@
 #include "search_algos.h"
 
+int search(int *array, size_t size, int value, size_t low, size_t high)
+{
+	size_t pos;
+
+	if (low > high)
+		return (-1);
+
+	pos = low + (((double)(high - low) / (array[high] - array[low])) *
+	(value - array[low]));
+
+	if (pos > size - 1)
+	{
+		printf("Value checked array[%ld] is out of range\n", pos);
+		return (-1);
+	}
+	printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+	if (array[pos] == value)
+		return (pos);
+	if (array[pos] < value)
+		return (search(array, size, value, pos + 1, high));
+	return (search(array, size, value, low, pos - 1));
+}
+
 /**
  * interpolation_search - searches for a value in a sorted array of integers
  * using the Interpolation search algorithm
@@ -12,23 +35,8 @@
 
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t pos, low = 0, high = size - 1;
-
 	if (!array || size == 0)
 		return (-1);
 
-	pos = low + (((double)(high - low) / (array[high] - array[low])) *
-	(value - array[low]));
-
-	if (pos > size)
-		printf("Value checked array[%ld] is out of range\n", pos);
-
-	for (; pos < size; pos++)
-	{
-		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
-		if (value == array[pos])
-			return (pos);
-	}
-
-	return (-1);
+	return (search(array, size, value, 0, size - 1));
 }
